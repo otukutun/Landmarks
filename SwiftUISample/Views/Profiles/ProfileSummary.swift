8c5cb9c7df59e958956acfa1c3cc787177d1d019
otukutun <otukutun@gmail.com>
2023-12-13 00:36:49 +0900

    Complete Working with UI controls.

---
+++ SwiftUISample/Views/Profiles/ProfileSummary.swift
@@ -0,0 +1,64 @@
+//
+//  ProfileSummary.swift
+//  SwiftUISample
+//
+//  Created by otukutun on 2023/12/11.
+//
+
+import SwiftUI
+
+struct ProfileSummary: View {
+    @Environment(ModelData.self) var modelData
+    var profile: Profile
+    
+    var body: some View {
+        ScrollView {
+            VStack(alignment: .leading, spacing: 10) {
+                Text(profile.username)
+                    .bold()
+                    .font(.title)
+                
+                Text("Notification: \(profile.prefersNotifications ? "On" : "OFF")")
+                Text("Seasonal Photos: \(profile.seasonalPhoto.rawValue)")
+                Text("Goal Date: ") + Text(profile.goalDate, style: .date)
+                
+                Divider()
+
+
+                VStack(alignment: .leading) {
+                    Text("Completed Badges")
+                        .font(.headline)
+
+
+                    ScrollView(.horizontal) {
+                        HStack {
+                            HikeBadge(name: "First Hike")
+                            HikeBadge(name: "Earth Day")
+                                .hueRotation(Angle(degrees: 90))
+                            HikeBadge(name: "Tenth Hike")
+                                .grayscale(0.5)
+                                .hueRotation(Angle(degrees: 45))
+                        }
+                        .padding(.bottom)
+                    }
+                }
+                
+                Divider()
+
+
+                VStack(alignment: .leading) {
+                    Text("Recent Hikes")
+                        .font(.headline)
+
+
+                    HikeView(hike: modelData.hikes[0])
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    ProfileSummary(profile: Profile.default)
+        .environment(ModelData())
+}
